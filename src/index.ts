c98ea3e9afac7b48695e9cf8ce23d443040d9467
Scott Dallinga <47750782+sdallinga@users.noreply.github.com>
2019-02-21 21:54:06 -0500

    Buttons working

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,48 @@
 import './styles.css';
-console.log('Ready to Party');
+import { ready } from './utils';
+
+//const button10 = document.getElementById('button10');
+//const button15 = document.getElementById('button15');
+//const button20 = document.getElementById('button20');
+//const button25 = document.getElementById('button25');
+
+ready(() => {
+    initialize_page();
+    console.log('Ready to Party');});
+
+function initialize_page() {
+    
+    setButtonDefaults();
+    cofigureEventListeners();
+}
+
+function setButtonDefaults()  { 
+
+    let btn = document.getElementById("button15");
+    btn.setAttribute( 'disabled', '');
+    console.log("Button defaults set");
+}
+
+function buttonClickEvent(e: Event) : void  {
+    enableAllButtons();
+    e.srcElement.setAttribute('disabled', '');
+}
+
+function enableAllButtons() {
+    var list : HTMLCollectionOf<HTMLButtonElement> = document.getElementsByTagName("button");
+        
+    for(var i=0; i< list.length; i++)
+    {
+        list[i].removeAttribute('disabled');
+    }
+}
+
+function cofigureEventListeners() {
+
+    var list : HTMLCollectionOf<HTMLButtonElement> = document.getElementsByTagName("button");      
+    for(var i=0; i< list.length; i++)
+    {
+        list[i].addEventListener('click', buttonClickEvent, false);
+    }
+}
+
