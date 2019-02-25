33fd8ddaa6eb38eace991251b254b510a092edfe
Scott Dallinga <sdallinga@gmail.com>
2019-02-25 14:43:12 -0500

    Removed unused spec files and renamed utils spec file.

---
+++ spec/utils_spec.ts
@@ -0,0 +1,17 @@
+import { roundToTwoPlaces } from "../src/utils";
+
+describe('Tests for roundToTwoPlaces function', () => {
+	it('Should return a string', () => {
+		const result = roundToTwoPlaces(1.234);
+		expect(typeof (result)).toBe('string');
+	});
+
+	it('Should round down for less than .5 pennies.', () => {
+		expect(roundToTwoPlaces(1.234)).toEqual('1.23');
+	});
+
+	it('Should round up for greater than or equal to .5 pennies', () => {
+		expect(roundToTwoPlaces(2.345)).toEqual('2.35');
+	});
+
+});
